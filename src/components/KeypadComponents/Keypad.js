712558eb5f1cb57013949d758bfafdf583f099ec
Mohit Kumar <mohitk02082000@gmail.com>
2020-10-31 01:08:21 +0530

    added functionality and validations to number input

--- src/components/KeypadComponents/Keypad.js
+++ src/components/KeypadComponents/Keypad.js
@@ -1,40 +1,40 @@
-import React from 'react'
-import Add from './functionKeys/Add'
-import Backspace from './functionKeys/Backspace'
-import Clear from './functionKeys/Clear'
-import Divide from './functionKeys/Divide'
-import Equals from './functionKeys/Equals'
-import Multiply from './functionKeys/Multiply'
-import Percent from './functionKeys/Percent'
-import Subtract from './functionKeys/Subtract'
-import KeyBase from './KeyBase'
+import React from "react";
+import Add from "./functionKeys/Add";
+import Backspace from "./functionKeys/Backspace";
+import Clear from "./functionKeys/Clear";
+import Divide from "./functionKeys/Divide";
+import Equals from "./functionKeys/Equals";
+import Multiply from "./functionKeys/Multiply";
+import Percent from "./functionKeys/Percent";
+import Subtract from "./functionKeys/Subtract";
 
-import "./Keypad.css"
+import "./Keypad.css";
+import NumberKey from "./NumberKey";
 const Keypad = () => {
-    return (
-        <div className="keypad-container">
-            <Clear />
-            <Percent />
-            <Backspace />
-            <Divide />
-            <KeyBase value={"7"} />
-            <KeyBase value={"8"} />
-            <KeyBase value={"9"} />
-            <Multiply />
-            <KeyBase value={"4"} />
-            <KeyBase value={"5"} />
-            <KeyBase value={"6"} />
-            <Subtract />
-            <KeyBase value={"1"} />
-            <KeyBase value={"2"} />
-            <KeyBase value={"3"} />
-            <Add />
-            <KeyBase value={"00"} />
-            <KeyBase value={"0"} />
-            <KeyBase value={"."} />
-            <Equals />
-        </div>
-    )
-}
+	return (
+		<div className="keypad-container">
+			<Clear />
+			<Percent />
+			<Backspace />
+			<Divide />
+			<NumberKey value={"7"} />
+			<NumberKey value={"8"} />
+			<NumberKey value={"9"} />
+			<Multiply />
+			<NumberKey value={"4"} />
+			<NumberKey value={"5"} />
+			<NumberKey value={"6"} />
+			<Subtract />
+			<NumberKey value={"1"} />
+			<NumberKey value={"2"} />
+			<NumberKey value={"3"} />
+			<Add />
+			<NumberKey value={"00"} />
+			<NumberKey value={"0"} />
+			<NumberKey value={"."} />
+			<Equals />
+		</div>
+	);
+};
 
-export default Keypad
+export default Keypad;
--- src/components/KeypadComponents/NumberKey.js
+++ src/components/KeypadComponents/NumberKey.js
@@ -1,9 +1,35 @@
 import React from "react";
+import {
+	useExpression,
+	useExpressionUpdater,
+} from "../../context/ExpressionContext";
 import KeyBase from "./KeyBase";
 
-const NumberKey = ({value}) => {
-    
-	return <KeyBase value={value} colored={false} />;
+const dotValidationAndExpressionUpdate = (exp, setExp) => {
+	const len = exp.length;
+	if (
+		exp[len - 1] === "%" ||
+		exp[len - 1] === "/" ||
+		exp[len - 1] === "*" ||
+		exp[len - 1] === "-" ||
+		exp[len - 1] === "+" ||
+		exp === ""
+	)
+		setExp((prev) => prev + "0.");
+	else setExp((prev) => prev + ".");
+};
+
+const NumberKey = ({ value }) => {
+	const expression = useExpression();
+	const setExpression = useExpressionUpdater();
+
+	const numberInputHandler = () => {
+		if (value === ".")
+			dotValidationAndExpressionUpdate(expression, setExpression);
+		else setExpression((prev) => prev + value);
+	};
+
+	return <KeyBase value={value} colored={false} onClick={numberInputHandler} />;
 };
 
 export default NumberKey;
