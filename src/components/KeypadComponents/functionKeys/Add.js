91f108b3fde1c8ab13871a48db5210ad85857a23
Mohit Kumar <mohitk02082000@gmail.com>
2020-10-31 23:15:54 +0530

    added functionality to add

--- src/components/KeypadComponents/functionKeys/Add.js
+++ src/components/KeypadComponents/functionKeys/Add.js
@@ -1,8 +1,33 @@
 import React from "react";
+import {
+	useExpression,
+	useExpressionUpdater,
+} from "../../../context/ExpressionContext";
 import KeyBase from "../KeyBase";
 
+const addValidation = (exp) => {
+	const len = exp.length;
+
+	if (exp === "") return false;
+	if (
+		exp[len - 1] === "*" ||
+		exp[len - 1] === "/" ||
+		exp[len - 1] === "-" ||
+		exp[len - 1] === "+"
+	)
+		return false;
+
+	return true;
+};
 const Add = () => {
-	return <KeyBase value="+" colored />;
+	const expression = useExpression();
+	const setExpression = useExpressionUpdater();
+
+	const addHandler = () => {
+		if (addValidation(expression)) setExpression((prev) => prev + "+");
+	};
+
+	return <KeyBase value="+" colored onClick={addHandler} />;
 };
 
 export default Add;
