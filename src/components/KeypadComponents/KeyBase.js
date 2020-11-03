57c6db437ea2be11aad29f706fcef2ffccf509de
Mohit Kumar <mohitk02082000@gmail.com>
2020-11-03 14:43:47 +0530

    made buttons non seletable
    made button highlight on click

--- src/components/KeypadComponents/KeyBase.js
+++ src/components/KeypadComponents/KeyBase.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./KeyBase.css";
 const KeyBase = (props) => {
 	const { value, onClick, colored } = props;
+	const [clicked, setClicked] = useState(false);
+
+	const handleClick = () => {
+		// 	const cover =document.querySelector(".cover")
+		// 	cover.classList.add("click-cover");
+		// 	setTimeout(()=>{
+		// 		cover.classList.remove("click-cover");
+		// 	},150)
+		setClicked(true);
+		setTimeout(() => {
+			setClicked(false);
+		}, 150);
+		onClick();
+	};
 
 	return (
-		<div className="keybase" onClick={onClick}>
-			<p className={colored ? "blue" : "default"}>{value}</p>
+		<div className="keybase" onClick={handleClick}>
+			<div className={clicked && "click-cover"}></div>
+			<p className={colored ? "blue color" : "default"}>{value}</p>
 		</div>
 	);
 };
